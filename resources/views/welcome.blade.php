7a1aeceab34697c1148833cbf76047239996b853
ayoub_alm <alayoub33@gmail.com>
2023-01-10 00:27:31 +0100

    init header 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,9 +71,7 @@
             font-family: "Playfair Display", Georgia, "Times New Roman", serif;
         }
 
-        .display-4 {
-            font-size: 2.5rem;
-        }
+
 
         @media (min-width: 768px) {
             .display-4 {
@@ -105,18 +103,7 @@
             font-size: .875rem;
         }
 
-        .card-img-right {
-            height: 100%;
-            border-radius: 0 3px 3px 0;
-        }
 
-        .flex-auto {
-            flex: 0 0 auto;
-        }
-
-        .h-250 {
-            height: 250px;
-        }
 
         @media (min-width: 768px) {
             .h-md-250 {
@@ -133,22 +120,6 @@
             border-radius: 2rem;
         }
 
-        /*
-        * Blog posts
-        */
-        .blog-post {
-            margin-bottom: 4rem;
-        }
-
-        .blog-post-title {
-            margin-bottom: .25rem;
-            font-size: 2.5rem;
-        }
-
-        .blog-post-meta {
-            margin-bottom: 1.25rem;
-            color: #727272;
-        }
 
         /*
         * Footer
@@ -173,30 +144,27 @@
         <header class="blog-header py-3">
             <div class="row flex-nowrap justify-content-between align-items-center">
                 <div class="col-4 pt-1">
-                    <a class="link-secondary" href="#">Subscribe</a>
+                    <a class="link-secondary" href="#">      <img src="{{ asset('/images/logo.jpg') }}" width="80px"></a>
                 </div>
-                <div class="col-4 text-center d-flex justify-content-center">
-
-                    <img src="{{ asset('/images/logo.jpg') }}" width="80px">
-
-                    <a class="blog-header-logo text-dark pt-2" href="#">  LTS  <br> ACADEMY</a>
+                <div class="col-4 text-center ">
+                    <a class="blog-header-logo text-dark pt-3 text-center" href="#">  LTS ACADEMY</a>
                 </div>
                 <div class="col-4 d-flex justify-content-end align-items-center">
-                    <a class="link-secondary" href="#" aria-label="Search">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor"
-                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img"
-                            viewBox="0 0 24 24">
-                            <title>Search</title>
-                            <circle cx="10.5" cy="10.5" r="7.5" />
-                            <path d="M21 21l-5.2-5.2" />
-                        </svg>
-                    </a>
+{{--                    <a class="link-secondary" href="#" aria-label="Search">--}}
+{{--                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor"--}}
+{{--                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img"--}}
+{{--                            viewBox="0 0 24 24">--}}
+{{--                            <title>Search</title>--}}
+{{--                            <circle cx="10.5" cy="10.5" r="7.5" />--}}
+{{--                            <path d="M21 21l-5.2-5.2" />--}}
+{{--                        </svg>--}}
+{{--                    </a>--}}
                     <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
                 </div>
             </div>
         </header>
 
-        <div class="nav-scroller py-1 mb-2">
+        <div class="nav-scroller py-1 mb-5">
             <nav class="nav d-flex justify-content-between">
                 <a class="p-2 link-secondary" href="#">World</a>
                 <a class="p-2 link-secondary" href="#">U.S.</a>
@@ -214,7 +182,7 @@
         </div>
 
     </div>
-    <div class="container" >
+    <div class="container mt-2" >
         <div class="row">
             <div class="col-lg-4 text-center ">
 
