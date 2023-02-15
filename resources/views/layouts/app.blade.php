8c996e010f517ccce5bbbe0b11a239b018ae0aa2
ayoub_alm <alayoub33@gmail.com>
2023-02-15 01:01:45 +0100

    modify nav bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -148,61 +148,76 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md" style="border-bottom: 0.5px rgba(0,0,0,0.3) solid;">
-            <div class="container-fluid">
-                <h2>
-                    <a class="navbar-brand d-" href="{{ url('/') }}">
-                        <img src="{{ asset('/images/logo.jpg') }}" width="80px">
-                        LTS ACADEMY
-                    </a>
-                </h2>
-
-
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="{{ asset('/images/ltsacademylogo.png') }}" alt="" width="60" height="60" class="d-inline-block align-text-top">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto row">
-{{--                            <li> home </li>--}}
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 flex-lg-row flex-sm-column" style="width: 100%;justify-content: space-around;">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">L'accueille </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Le Center</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+
+{{--                        <ul class="navbar-nav ms-auto">--}}
+                                                    <!-- Authentication Links -->
+                                                    @guest
+                                                        @if (Route::has('login'))
+                                                            <li class="nav-item">
+                                                                <a class="nav-link " href="{{ route('login') }}">{{ __("S'inscrire") }}</a>
+                                                            </li>
+                                                        @endif
+
+                                                        @if (Route::has('register'))
+                                                            <li class="nav-item">
+                                                                <a class="nav-link " href="{{ route('register') }}">{{ __("s'enregistrer") }}</a>
+                                                            </li>
+                                                        @endif
+                                                    @else
+                                                        <li class="nav-item dropdown">
+                                                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                                                {{ Auth::user()->name }}
+                                                            </a>
+
+                                                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                                                   onclick="event.preventDefault();
+                                                                                 document.getElementById('logout-form').submit();">
+                                                                    {{ __('Logout') }}
+                                                                </a>
+
+                                                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                                    @csrf
+                                                                </form>
+                                                            </div>
+                                                        </li>
+                                                    @endguest
+{{--                                                </ul>--}}
                     </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link btn btn-sm btn-outline-secondary" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link btn btn-sm btn-outline-secondary" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
                 </div>
             </div>
         </nav>
