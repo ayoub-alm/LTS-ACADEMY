f83a66a886f9ab6629bae3632904babb80809f58
ayoub-alm <=>
2023-05-14 22:22:49 +0100

    add image to about us

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -65,7 +65,7 @@
         <h5 class="display-3 p-2 fw-bolder mb-4 text-center">
             <span class="red-color fw-bolder">Qui sommes nous </span>
         </h5>
-            <div class="row featurette">
+            <div class="row featurette mt-3">
                 <div class="col-md-6 my-auto">
                     <p class="lead p-lg-2 my-auto lh-lg">LTS Academy est un centre de formation et de langues de renom, offrant une expérience d'apprentissage exceptionnelle à ses étudiants. Avec une équipe d'enseignants hautement qualifiés et expérimentés, LTS Academy offre des programmes de formation de haute qualité dans une variété de domaines.</p>
                 </div>
@@ -75,7 +75,7 @@
                 </div>
             </div>
 
-            <div class="row featurette">
+            <div class="row featurette mt-3">
                 <div class="col-md-7 order-md-2 my-auto">
                     <p class="lead p-lg-2 my-auto lh-lg">Que ce soit pour améliorer vos compétences linguistiques, développer vos compétences professionnelles, ou simplement pour explorer de nouveaux intérêts et passions, LTS Academy a le programme parfait pour vous. Les cours sont dispensés dans un environnement d'apprentissage dynamique et interactif, avec des ressources et des technologies de pointe à votre disposition</p>
                 </div>
@@ -87,7 +87,7 @@
             </div>
 
 
-            <div class="row featurette">
+            <div class="row featurette mt-3">
                 <div class="col-md-7 my-auto my-auto">
                     <p class="lead p-lg-2 my-auto lh-lg">En plus de l'enseignement en classe, LTS Academy propose également des programmes de formation en ligne, ce qui permet une flexibilité et une accessibilité accrues pour les étudiants. Avec une approche personnalisée pour chaque apprenant, LTS Academy est déterminé à vous aider à atteindre vos objectifs d'apprentissage de manière efficace et agréable. Rejoignez-nous dès maintenant pour une expérience d'apprentissage inoubliable !</p>
                 </div>
