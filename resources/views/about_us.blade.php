70912b4178eb8b9a0b2a6fccdf4cdfe29d390945
ayoub-alm <=>
2023-05-14 22:20:53 +0100

    add image to about us

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -67,7 +67,7 @@
         </h5>
             <div class="row featurette">
                 <div class="col-md-6 my-auto">
-                    <p class="lead p-lg-5 my-auto lh-lg">LTS Academy est un centre de formation et de langues de renom, offrant une expérience d'apprentissage exceptionnelle à ses étudiants. Avec une équipe d'enseignants hautement qualifiés et expérimentés, LTS Academy offre des programmes de formation de haute qualité dans une variété de domaines.</p>
+                    <p class="lead p-lg-2 my-auto lh-lg">LTS Academy est un centre de formation et de langues de renom, offrant une expérience d'apprentissage exceptionnelle à ses étudiants. Avec une équipe d'enseignants hautement qualifiés et expérimentés, LTS Academy offre des programmes de formation de haute qualité dans une variété de domaines.</p>
                 </div>
                 <div class="col-md-6">
                     <img src="https://www.lecoindesentrepreneurs.fr/wp-content/uploads/2020/01/cr%C3%A9er-une-soci%C3%A9t%C3%A9-de-formation.png" width="500" height="500" class="img-fluid">
@@ -77,10 +77,10 @@
 
             <div class="row featurette">
                 <div class="col-md-7 order-md-2 my-auto">
-                    <p class="lead p-lg-5 my-auto lh-lg">Que ce soit pour améliorer vos compétences linguistiques, développer vos compétences professionnelles, ou simplement pour explorer de nouveaux intérêts et passions, LTS Academy a le programme parfait pour vous. Les cours sont dispensés dans un environnement d'apprentissage dynamique et interactif, avec des ressources et des technologies de pointe à votre disposition</p>
+                    <p class="lead p-lg-2 my-auto lh-lg">Que ce soit pour améliorer vos compétences linguistiques, développer vos compétences professionnelles, ou simplement pour explorer de nouveaux intérêts et passions, LTS Academy a le programme parfait pour vous. Les cours sont dispensés dans un environnement d'apprentissage dynamique et interactif, avec des ressources et des technologies de pointe à votre disposition</p>
                 </div>
                 <div class="col-md-5 order-md-1">
-                    <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+{{--                    <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>--}}
                     <img src="https://www.lecoindesentrepreneurs.fr/wp-content/uploads/2020/01/cr%C3%A9er-une-soci%C3%A9t%C3%A9-de-formation.png" width="500" height="500" class="img-fluid">
 
                 </div>
@@ -89,7 +89,7 @@
 
             <div class="row featurette">
                 <div class="col-md-7 my-auto my-auto">
-                    <p class="lead p-lg-5 my-auto lh-lg">En plus de l'enseignement en classe, LTS Academy propose également des programmes de formation en ligne, ce qui permet une flexibilité et une accessibilité accrues pour les étudiants. Avec une approche personnalisée pour chaque apprenant, LTS Academy est déterminé à vous aider à atteindre vos objectifs d'apprentissage de manière efficace et agréable. Rejoignez-nous dès maintenant pour une expérience d'apprentissage inoubliable !</p>
+                    <p class="lead p-lg-2 my-auto lh-lg">En plus de l'enseignement en classe, LTS Academy propose également des programmes de formation en ligne, ce qui permet une flexibilité et une accessibilité accrues pour les étudiants. Avec une approche personnalisée pour chaque apprenant, LTS Academy est déterminé à vous aider à atteindre vos objectifs d'apprentissage de manière efficace et agréable. Rejoignez-nous dès maintenant pour une expérience d'apprentissage inoubliable !</p>
                 </div>
                 <div class="col-md-5">
 {{--                    <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>--}}
