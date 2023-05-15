071781e7c1d177eb43c5d4d5751967726587a3dd
ayoub-alm <=>
2023-05-15 02:54:25 +0100

    add image to about us

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -160,29 +160,29 @@
     </style>
 
 
-    <script>
-        const swiper = new Swiper('.swiper', {
-            // Optional parameters
-            direction: 'vertical',
-            loop: true,
+{{--    <script>--}}
+{{--        const swiper = new Swiper('.swiper', {--}}
+{{--            // Optional parameters--}}
+{{--            direction: 'vertical',--}}
+{{--            loop: true,--}}
 
-            // If we need pagination
-            pagination: {
-                el: '.swiper-pagination',
-            },
+{{--            // If we need pagination--}}
+{{--            pagination: {--}}
+{{--                el: '.swiper-pagination',--}}
+{{--            },--}}
 
-            // Navigation arrows
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
+{{--            // Navigation arrows--}}
+{{--            navigation: {--}}
+{{--                nextEl: '.swiper-button-next',--}}
+{{--                prevEl: '.swiper-button-prev',--}}
+{{--            },--}}
 
-            // And if we need scrollbar
-            scrollbar: {
-                el: '.swiper-scrollbar',
-            },
-        });
-    </script>
+{{--            // And if we need scrollbar--}}
+{{--            scrollbar: {--}}
+{{--                el: '.swiper-scrollbar',--}}
+{{--            },--}}
+{{--        });--}}
+{{--    </script>--}}
 </head>
 <body>
     <div id="app">
